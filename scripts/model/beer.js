2b81a81442fd89d95c9261a31addec414218a7f2
Katy Robinson <katyorob@gmail.com>
2018-01-29 17:10:57 -0800

    added images, working on beer.js and food.js

---
+++ scripts/model/beer.js
@@ -0,0 +1,13 @@
+'use strict';
+
+(function (module) = {
+    function Beer (rawBeerData) {
+        this.style = style;
+        this.name = name;
+        this.description = description;
+        this.foodPairing = foodPairing;
+    }
+
+
+    module.Beer = Beer;
+})(app)
