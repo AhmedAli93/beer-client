89ac93e68e3f06365c048936a736b3a39285f11b
Katy Robinson <katyorob@gmail.com>
2018-01-30 13:56:27 -0800

    server.js working with api

--- server.js
+++ server.js
@@ -6,44 +6,46 @@ const cors = require('cors');
 const pg = require('pg');
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const requestProxy = require('express-request-proxy');
+const superagent = require('superagent');
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 const CLIENT_URL = process.env.CLIENT_URL;
 
 const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 client.on('error', err => console.error(err));
+
 app.use(cors());
-
-
-// Brewery API proxy
-function proxyBreweryAPI(req, res) {
-    console.log(req.params[0]);
-    var baseUrl = ``;
-    console.log(baseUrl);
-    (requestProxy({
-        url: baseUrl
-    }))(req, res);
-}
-
 app.use(express.static('./public'));
 
 app.get('/', (req, res) => {
   res.sendFile('index.html', { root: './public'});
 });
 
-app.get('/beers/*', proxyBreweryAPI);
+// breweryDB proxy call - /beers/:beerId gets a specific beer
+app.get('/beers/*', beers);
+function beers(request, response){
+  superagent
+  .get(`http://api.brewerydb.com/v2/style/${request.params[0]}`)
+  .set('Authorization', process.env.BREWERY_SECRET)
+  .end(function(err, res){ 
+    response.send(JSON.parse(res.text).data);
+  })
+}
 
-//endpoints
-app.get('/testing', (req, res) => {
-    res.send('WERK WERK WERK');
-});
+// ::::  if you want to get all beers, get a premium acct and get all beers :::: //
 
-//getting foods from database
+app.get('/beers', allBeers);
+function allBeers(request, response){
+  superagent
+  .get(`http://api.brewerydb.com/v2/beers`)
+  .set('Authorization', process.env.BREWERY_SECRET)
+  .end(function(err, res){ 
+    response.send(JSON.parse(res.text).data);
+  })
+}
 
-
-
-//getting beers from api
-
+// getting foods from database
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
