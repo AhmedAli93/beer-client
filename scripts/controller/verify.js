ad0ad86dc234bc9951d657eeee16532b90918903
Wyatt  pefley <wyattpefley@Wyatts-MacBook-Pro.local>
2018-01-29 17:14:00 -0800

    changed verify.js and popup

--- scripts/controller/verify.js
+++ scripts/controller/verify.js
@@ -1,13 +1,24 @@
+'use strict';
+// This is the event handler for the button to access our page, this will fade in our verify popup.
 $('.button').click(function(event) {
     $('.popup').fadeIn(800);
   });
-  // popup close function
+// Event handler for when the close button is pushed.
   $('.close_button').click(function(event) {
     $('.popup').fadeOut(800);
   });
-  // popup 'esc' key to close
+  $('.yup').click(function(event){
+    $('.popup').fadeOut(800);
+    $('.popup2').fadeIn(800);
+    console.log('User is of age:', true);
+  });
+// This is an event handler that listens for a keypress of the escape key, this will also close the
   $(document).on('keydown', function(e) {
-    if (e.keyCode === 27) { // ESC
+    if (e.keyCode === 27) {
       $('.popup').hide();
     }
+  });
+$('.nope').click(function(event){
+    $('.popup').fadeOut(800);
+    console.log('User is of age:', false);
   });
